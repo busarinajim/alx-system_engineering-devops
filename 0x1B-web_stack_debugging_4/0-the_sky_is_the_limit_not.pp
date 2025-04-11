b65a25e40245e7ade617933f17546800dc359ae9
Najim <busarinajim>
2025-04-11 18:24:30 +0100

    Fix Nginx overload using Puppet

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -3,14 +3,42 @@
 # Increase ulimit for Nginx user
 file { '/etc/security/limits.conf':
   ensure  => file,
-  content => template('nginx/limits.conf.erb'),
+  content => "# Increase file descriptor limit for Nginx\nwww-data soft nofile 65535\nwww-data hard nofile 65535\n",
   mode    => '0644',
 }
 
 # Ensure Nginx configuration allows more connections
 file { '/etc/nginx/nginx.conf':
   ensure  => file,
-  content => template('nginx/nginx.conf.erb'),
+  content => @('END_OF_NGINX_CONF')
+user www-data;
+worker_processes auto;
+pid /run/nginx.pid;
+
+events {
+    worker_connections 4096; # Increased from default
+    multi_accept on;
+}
+
+http {
+    sendfile on;
+    tcp_nopush on;
+    tcp_nodelay on;
+    keepalive_timeout 65;
+    types_hash_max_size 2048;
+
+    include /etc/nginx/mime.types;
+    default_type application/octet-stream;
+
+    access_log /var/log/nginx/access.log;
+    error_log /var/log/nginx/error.log;
+
+    gzip on;
+
+    include /etc/nginx/conf.d/*.conf;
+    include /etc/nginx/sites-enabled/*;
+}
+| END_OF_NGINX_CONF
   mode    => '0644',
   notify  => Service['nginx'],
 }
@@ -20,12 +48,4 @@ service { 'nginx':
   ensure     => running,
   enable     => true,
   hasrestart => true,
-}
-
-# Execute command to apply ulimit change to running session (optional for immediate effect)
-exec { 'apply-ulimit':
-  command     => 'ulimit -n 65535',
-  path        => ['/bin', '/usr/bin'],
-  refreshonly => true,
-  subscribe   => File['/etc/security/limits.conf'],
 }
