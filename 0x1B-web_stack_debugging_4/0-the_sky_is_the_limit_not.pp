be7284c6f2d5e1399e4738e2a8e4713463e98030
Najim <busarinajim>
2025-04-11 18:28:10 +0100

    Fix Nginx overload using Puppet

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,51 +1,6 @@
-# Puppet manifest to fix Nginx failing under high load by increasing ulimit and worker connections
+# This manifest fixes Nginx configuration to handle high traffic properly
 
-# Increase ulimit for Nginx user
-file { '/etc/security/limits.conf':
-  ensure  => file,
-  content => "# Increase file descriptor limit for Nginx\nwww-data soft nofile 65535\nwww-data hard nofile 65535\n",
-  mode    => '0644',
-}
-
-# Ensure Nginx configuration allows more connections
-file { '/etc/nginx/nginx.conf':
-  ensure  => file,
-  content => @('END_OF_NGINX_CONF')
-user www-data;
-worker_processes auto;
-pid /run/nginx.pid;
-
-events {
-    worker_connections 4096; # Increased from default
-    multi_accept on;
-}
-
-http {
-    sendfile on;
-    tcp_nopush on;
-    tcp_nodelay on;
-    keepalive_timeout 65;
-    types_hash_max_size 2048;
-
-    include /etc/nginx/mime.types;
-    default_type application/octet-stream;
-
-    access_log /var/log/nginx/access.log;
-    error_log /var/log/nginx/error.log;
-
-    gzip on;
-
-    include /etc/nginx/conf.d/*.conf;
-    include /etc/nginx/sites-enabled/*;
-}
-| END_OF_NGINX_CONF
-  mode    => '0644',
-  notify  => Service['nginx'],
-}
-
-# Ensure Nginx service is running and restarts if config changes
-service { 'nginx':
-  ensure     => running,
-  enable     => true,
-  hasrestart => true,
+exec { 'fix--for-nginx':
+  command => 'sed -i "/^worker_processes/c\worker_processes auto;" /etc/nginx/nginx.conf && service nginx restart',
+  path    => '/usr/bin:/usr/sbin:/bin:/sbin',
 }
