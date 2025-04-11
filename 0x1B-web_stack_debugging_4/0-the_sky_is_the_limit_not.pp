eb809d7f7d038776dc79b5397992cdb3684342ae
Najim <busarinajim>
2025-04-11 17:31:57 +0100

    Fix Nginx overload using Puppet

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,7 +1,12 @@
-# This manifest fixes Nginx configuration to handle high traffic properly
+# This manifest configures Nginx to handle high traffic by setting worker_processes to auto
 
-exec { 'fix--for-nginx':
-  command => 'sed -i "/^worker_processes/c\worker_processes auto;" /etc/nginx/nginx.conf && service nginx restart',
+exec { 'fix_nginx_config':
+  command => 'sed -i "s/worker_processes [0-9]*/worker_processes auto/" /etc/nginx/nginx.conf',
   path    => '/usr/bin:/usr/sbin:/bin:/sbin',
 }
 
+exec { 'restart_nginx':
+  command => 'service nginx restart',
+  path    => '/usr/bin:/usr/sbin:/bin:/sbin',
+  require => Exec['fix_nginx_config'],
+}
